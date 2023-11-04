41729ed8faed49b8a241599ffd5d4ef9d2e0f308
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-11-03 23:24:20 -0300

    feat(tryCatchCustom) DomainExceptions extends Exception

---
+++ src/tryCatchOne/exceptions/DomainExceptions.java
@@ -0,0 +1,10 @@
+package tryCatchOne.exceptions;
+
+public class DomainExceptions extends Exception{
+
+  private static final long serialVersionUUID = 1L;
+
+  public DomainExceptions(String msg) {
+    super(msg);
+  }
+}
