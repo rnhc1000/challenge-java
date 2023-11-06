11aa9683056bef97d4ac22e4ca266abe991d60a1
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-11-06 18:58:33 -0300

    feat(Predicate) interface implementation

---
+++ src/predicate/util/ProductPredicate.java
@@ -0,0 +1,13 @@
+package predicate.util;
+
+import predicate.entities.Product;
+
+import java.util.function.Predicate;
+
+public class ProductPredicate implements Predicate<Product> {
+
+  @Override
+  public boolean test(Product product) {
+    return product.getPrice() >= 100.0;
+  }
+}
