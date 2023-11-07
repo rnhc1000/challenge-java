d36469cecd2b4d469a181309f6d09173702c6cd1
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-11-06 23:33:48 -0300

    feat(Function as Argument) method loosely coupled to ProductsService class thru Predicate as an argument

--- src/functionAsArgument/application/Solution.java
+++ src/functionAsArgument/application/Solution.java
@@ -12,10 +12,14 @@ public class Solution {
     list.add(new Products("Mouse", 50.00));
     list.add(new Products("Tablet", 350.50));
     list.add(new Products("HD Case", 80.90));
+    list.add(new Products("Tv LED", 1900.00));
+    list.add(new Products("Mechanical Mouse", 50.00));
+    list.add(new Products("Ipad Tablet", 350.50));
+    list.add(new Products("Monitor", 80.90));
 
-    double sum = new ProductsService().filteredSum(list);
+    double sum = new ProductsService().filteredSum(list, p->p.getName().charAt(0)=='M');
 
-    System.out.printf("%.2f%n",sum);
+    System.out.printf("Sum: %.2f",sum);
 
 
 
--- src/functionAsArgument/services/ProductsService.java
+++ src/functionAsArgument/services/ProductsService.java
@@ -3,13 +3,14 @@ package functionAsArgument.services;
 import functionAsArgument.entities.Products;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class ProductsService {
-  public double filteredSum(List<Products> list) {
+  public double filteredSum(List<Products> list, Predicate<Products> criteria) {
 
     double sum = 0.0;
     for (Products product : list) {
-      if (product.getName().charAt(0) == 'T') {
+      if (criteria.test(product)) {
         sum += product.getPrice();
       }
     }
