ba880ef3783c53bbee8f955329c9cb49a55755d7
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-11-07 15:20:06 -0300

    feat(StreamChallengeOne) as promised get avg price running thru only one stream and print the list using <nameOfList>.forEach(System.out::println)

--- src/streamChallengeOne/application/Solution.java
+++ src/streamChallengeOne/application/Solution.java
@@ -7,8 +7,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Read a file that has a list of products
@@ -16,7 +16,11 @@ import java.util.stream.Collectors;
  * Goal: Return the average price of the products
  * Goal: Return a list of products with prices less
  * than the average...
+ * Hint:
+ * filter(x->x.getPrice < average).map(Productx::getName)
+ *
  */
+
 public class Solution {
   public static void main(String[] args) {
 
@@ -29,10 +33,26 @@ public class Solution {
 
       while ((line = br.readLine()) != null) {
 
-        String [] lines = line.split(",");
+        String[] lines = line.split(",");
         list.add(new Productx(lines[0], Double.parseDouble(lines[1])));
 
       }
+
+      Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+
+      Double average = list.stream().
+              map(Productx::getPrice)
+              .reduce(0.0, (x, y) -> x + y)
+              / list.size();
+
+      List<String> smallestPrices = list.stream()
+              .filter(x -> x.getPrice() < average)
+              .map(Productx::getName)
+              .sorted(comp.reversed()).toList();
+
+      System.out.println("Average price: " + String.format("%.2f", average));
+
+      smallestPrices.forEach(System.out::println);
 
     } catch (FileNotFoundException e) {
       System.out.println("Error: " + e.getMessage());
@@ -41,19 +61,7 @@ public class Solution {
       System.out.println();
     }
 
-    for (Productx p : list) {
-      System.out.println(p);
-    }
-
-    Double sum = list.stream().map(Productx::getPrice).reduce(0.0, (x,y)->x+y);
-    int avg = (int) list.stream().map(Productx::getPrice).count();
-    double average = (double)sum/avg;
-
-    List<String> l = list.stream().filter(x -> x.getPrice()<average).map(Productx::getName).sorted().toList();
-    System.out.println("Average price: " + String.format("%.2f",average));
-    for (String cc : l){
-      System.out.println(cc);
-    }
-
   }
+
 }
+
